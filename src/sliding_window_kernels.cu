2b0b7ba156fa73fcf48ed9c429c4f12ad5d23b4d
Glenn Hazelwood <hazelnutvt04@gmail.com>
2020-12-29 02:35:31 -0500

    vectorized load with float4 won't work, since consecutive windows need to overlap and address alignment won't allow for that

--- src/sliding_window_kernels.cu
+++ src/sliding_window_kernels.cu
@@ -84,51 +84,11 @@ __global__ void sliding_window_vectorized_loads(
       window_sums[index + 1] = temp1;
    } 
 
-
-   //for ( int index = global_index; index < num_windowed_samples/2; index+=stride ) {
-      
-   //   float4 t_two_window_sums = make_float4( 0.f, 0.f, 0.f, 0.f );
-
-   //   for ( int w_index = 0; w_index < window_size/2; ++w_index ) {
-   //      if ( ( threadIdx.x == 0 ) && ( blockIdx.x == 0) ) { 
-   //         printf( "\tblockIdx.x is %d, index is %d and w_index is %d, t_two_window_sums before is { %f, %f, %f, %f }\n", 
-   //            blockIdx.x, index, w_index,
-   //            t_two_window_sums.x, t_two_window_sums.y, t_two_window_sums.z, t_two_window_sums.w );
-   //      }
-
-   //      t_two_window_sums += reinterpret_cast<float4*>(samples)[index + w_index];
-         
-   //      if ( ( threadIdx.x == 0 ) && ( blockIdx.x == 0) ) { 
-   //         printf( "\tblockIdx.x is %d, index is %d and w_index is %d, samples[%d + %d] as float4 is { %f, %f, %f, %f }\n", 
-   //            blockIdx.x, index, w_index, index, w_index,
-   //            reinterpret_cast<float4*>(samples)[index + w_index].x,
-   //            reinterpret_cast<float4*>(samples)[index + w_index].y,
-   //            reinterpret_cast<float4*>(samples)[index + w_index].z,
-   //            reinterpret_cast<float4*>(samples)[index + w_index].w
-   //         );
-   //         printf( "\tblockIdx.x is %d, index is %d and w_index is %d, t_two_window_sums after is { %f, %f, %f, %f }\n\n", 
-   //            blockIdx.x, index, w_index,
-   //            t_two_window_sums.x, t_two_window_sums.y, t_two_window_sums.z, t_two_window_sums.w );
-   //      }
-
-   //   }
-      
-   //   reinterpret_cast<float4*>(window_sums)[index] = t_two_window_sums;
-   //} // end of for ( int index = global_index; index < num_windowed_samples/2; index+=stride ) {
-
-   //if ( ( global_index == num_windowed_samples/2 ) && ( ( num_windowed_samples & 1 ) == 1 ) ) {
-      
-   //   cufftComplex t_window_sum = samples[global_index];
-
-   //   for( int w_index = 1; w_index < window_size; ++w_index ) {
-   //      t_window_sum = cuCaddf( t_window_sum, samples[global_index + w_index] );
-   //   }
-
-   //   window_sums[global_index] = t_window_sum;
-   //}
 } // end of __global__ void sliding_window_vectorized_loads
 
 
+// Can't work due to float4 aligment making it impossible to 
+// have consecutive windows overlap
 __global__ void sliding_window_vectorized_loads2(
    cufftComplex* __restrict__ window_sums, 
    cufftComplex* const __restrict__ samples, 
@@ -143,14 +103,31 @@ __global__ void sliding_window_vectorized_loads2(
    for( int index = global_index; index < num_windowed_samples/2; index+=stride ) {
       float4 temp = make_float4( 0.f, 0.f, 0.f, 0.f );
       float2 temp2 = make_float2( 0.f, 0.f ); 
-      for ( int w_index = 1; w_index < window_size; w_index +=2 ) {
-         temp += reinterpret_cast<float4*>(samples)[index + w_index];
-         temp2.x += temp.x + temp.z;
-         temp2.y += temp.y + temp.w;
+      
+      for( int w_index = 0; w_index < window_size/2; ++w_index ) {
+         if ( ( blockIdx.x == 0 ) && ( threadIdx.x == 1 ) ) {
+            printf( "\t(float4) samples[%d + %d] = { %f, %f, %f, %f }\n", 
+               index, w_index,
+               reinterpret_cast<float4*>(samples)[index + w_index].x,
+               reinterpret_cast<float4*>(samples)[index + w_index].y,
+               reinterpret_cast<float4*>(samples)[index + w_index].z,
+               reinterpret_cast<float4*>(samples)[index + w_index].w
+            );
+         }
+
+         temp = reinterpret_cast<float4*>(samples)[index + w_index];
+
+         temp2.x += temp.x;
+         temp2.x += temp.z;
+         temp2.y += temp.y;
+         temp2.y += temp.w;
+         
+         if ( ( blockIdx.x == 0 ) && ( threadIdx.x == 1 ) ) {
+            printf( "\t(float2) temp2 = { %f, %f }\n\n", temp2.x, temp2.y );
+         }
       }
 
       //reinterpret_cast<float4*>(window_sums)[index] = temp;
-      window_sums[index] = temp2;
       window_sums[index] = temp2;
    } 
 }
